29ddeb7e21e158b76a17868a37dc6815ee79ddf9
tmose1106 <tmoseley1106@gmail.com>
2016-05-01 00:22:04 -0400

    Added initial GTK interface
    After some long and hard work, Ian Swerve and I have formulated a basic interface for Tadman. And so far, it is really great! It contains most of the features I initially had in mind. There are still a few things missing though:
        + A search feature, which does not look solely at the beginning of the line.
        + The ability to add a phrase to the end of an option, especially those which have an equals on their rear, which can be passed multiple different options.
        + Another page with entry boxes for editing standard install directories.
        + The name of the program you are configuring in the information panel.
    
    But all in all, progress is moving along quite nicely. Until next time!
                                    ~ Love, Ted M.

--- README.md
+++ README.md
@@ -8,17 +8,18 @@ This is a (universal?) package manager that is currently in developement. It is 
 
 The only dependencies so far are:
 + Python 3 (_obviously_)
-+ click, a Python CLI module (You can find it [here](http://click.pocoo.org/6/).)
++ click, a Python CLI module (You can find it [here](http://click.pocoo.org/6/))
++ pygobject, for the GTK+ 3 interface
 
 Some optional dependencies are:
-+ CMake, to build CMake projects (You can find it [here](https://cmake.org/).)
++ CMake, to build CMake projects (You can find it [here](https://cmake.org/))
 
 ### Execution
 
 Tadman should make building from source code easier. Right now, there is no system in place to build a Tadman executable or even install the scripts. Until then, you can test it by running the following:
 
-    $ git clone https://github.com/KeepPositive/Tadman.git
-    $ cd Tadman
-    $ python3 tadman_main.py [PATH_TO_SOURCE_CODE]'
+$ git clone https://github.com/KeepPositive/Tadman.git
+$ cd Tadman
+$ python3 tadman_main.py [PATH_TO_SOURCE_CODE]'
 
-It _should_ print out some small lists of the configure options available for the program you wish to install. A way to edit these options and run the configure script is coming soon. 
+It _should_ open a small list of configuration options available for the program you wish to install. Try checking some of them off and then hitting the run button! It should print a list of the options you just chose, no matter if it is Autotools or CMake based.
--- tadman_main.py
+++ tadman_main.py
@@ -12,16 +12,21 @@ import autotools_config_proc
 import cmake_config_write
 import cmake_config_read
 import cmake_config_proc
+import gtk_interface
 
 @click.command()
+@click.option('--dump', '-d', multiple=True, is_flag=True, 
+              help="Print raw options list to stdout (for debugging)")
 @click.option('--remove', '-r', multiple=True, is_flag=True, 
-              help="Remove previous config_out.txt file.")
+              help="Remove previous config_out.txt file")
 @click.argument('path')
-def main(path, remove):
+def main(path, remove, dump):
 
     configure_file_path = "%s/configure" % path
     cmakelists_file_path = "%s/CMakeLists.txt" % path
     config_out_file_path = "%s/config_out.txt" % path
+    filtered_list = ()
+    build_type = ()
    
     if remove:
         try:
@@ -32,21 +37,23 @@ def main(path, remove):
 
     if os.path.isfile(configure_file_path):
 
+        build_type = 'autotools'
+
         if os.path.isfile(config_out_file_path):
             print("Previous config_out.txt found!")
             config_options_list = autotools_config_read.make_options_list(config_out_file_path)
-            filt_list = autotools_config_proc.autotool_new_processor(config_options_list)
+            filtered_list = autotools_config_proc.autotool_new_processor(config_options_list)
         else:
             autotools_config_write.write_config_txt(path)
             config_options_list = autotools_config_read.make_options_list(config_out_file_path)
-            filt_list = autotools_config_proc.autotool_new_processor(config_options_list)
-
-        for x in filt_list:
-            print(x)
+            filtered_list = autotools_config_proc.autotool_new_processor(config_options_list)
 
     elif os.path.isfile(cmakelists_file_path):
-        print("Found CMake file.")
-        print("WARNING: CMake configuration requires dependencies installed prior to config.")
+
+        build_type = 'cmake'
+        
+        print("WARNING: CMake configuration requires dependencies installed prior to config.", 
+              file=sys.stderr)
 
         if os.path.isfile(config_out_file_path):
             print("Previous config_out.txt found!")
@@ -56,13 +63,17 @@ def main(path, remove):
             cmake_config_write.write_config_txt(path)
             config_options_list = cmake_config_read.make_options_list(config_out_file_path)
             filtered_list = cmake_config_proc.cmake_processor(config_options_list)
-
-        for y in filtered_list:
-            print(y)
-
+    
     else:
         print("This directory does not have a configure file.")
         sys.exit(97)
+
+    if dump:
+        print(config_options_list)
+
+    interface = gtk_interface.gui_main(build_type, filtered_list)
+    # Print the options once the GUI closes.
+    print(interface)
 
 if __name__ == '__main__':
     main()
